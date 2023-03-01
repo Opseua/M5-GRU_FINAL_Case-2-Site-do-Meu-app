00742f2d51dbf652e9112bb40ea42a3b7f7cf552
Gaby França <gabriellyfranca810@gmail.com>
2023-02-28 23:24:01 -0300

    feat: Finalizada Navbar e footer
    AJUSTE DE CONFIGURACOES DO PROJETO

---
+++ src/Pages/Sobre.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Footer from "../components/Footer/Footer";
+import Navbar from "../components/Navbar/Navbar";
+
+export default function Sobre() {
+  return (
+    <div>
+      <Navbar />
+      <h1>Sobre</h1>
+      <Footer />
+    </div>
+  );
+}
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <>
+      <footer className="footer">
+        <p className="footer-by">Caderno da Saúde &#169;</p>
+      </footer>
+    </>
+  );
+}
