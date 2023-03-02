dfc4d2de6d2e097510aa692d23de4d48275e1b34
matheuszack <113534912+matheuszack@users.noreply.github.com>
2023-03-02 18:25:34 -0300

    Add files via upload

--- src/views/getnotas/GetNotas.jsx
+++ src/views/getnotas/GetNotas.jsx
@@ -2,13 +2,14 @@ import Footer from "../../components/Footer/Footer";
 import Navbar from "../../components/Navbar/Navbar";
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import './GetNotas.css';
 
 
 function GetNotas() {
   const [data, setData] = useState([]);
 
   useEffect(() => {
-    axios.get('https://m5-gru-crud-api.cyclic.app/usuarios')
+    axios.get('https://m5-gru-crud-api.cyclic.app/notas')
       .then(response => setData(response.data.data))
       .catch(error => console.log(error));
   }, []);
@@ -17,10 +18,10 @@ function GetNotas() {
     <div>
       <Navbar />
       {data.map(item => (
-        <div key={item.usuario_nome}>
-          <h2>{item.usuario_nome}</h2>
-          <p>{item.usuario_email}</p>
-          <p>{item.usuario_reset_resposta}</p>
+        <div class="blocosN" key={item.nota_titulo}>
+          <h2>{item.nota_titulo}</h2>
+          <p class = "textos1">{item.nota_informacao}</p>
+          <p>{item.nota_ultima_edicao}</p>
         </div>
       ))}
       <Footer />
