d713b4eebd883284e6b8a0f62b79b269b1c8ce1f
matheuszack <113534912+matheuszack@users.noreply.github.com>
2023-03-04 01:01:38 -0300

    Delete src directory

--- src/components/Footer/Footer.jsx
+++
@@ -1,9 +0,0 @@
-export default function Footer() {
-  return (
-    <>
-      <footer className="footer">
-        <p className="footer-by">Caderno da Saúde &#169;</p>
-      </footer>
-    </>
-  );
-}
