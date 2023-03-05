608c8f8081ded0a907d338188279010e06b15fcd
matheuszack <113534912+matheuszack@users.noreply.github.com>
2023-03-05 17:46:54 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import { HashRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar/Navbar";
+import Footer from "./components/Footer/Footer";
+import Inicio from "./views/inicio/Inicio";
+import Sobre from "./views/sobre/Sobre";
+import Funcionalidades from "./views/funcionalidades/Funcionalidades";
+import Comunicados from "./views/comunicados/comunicados";
+import Login from "./views/login/Login";
+import Teste from "./views/teste/Teste";
+import Perfil from "./views/perfil/perfil";
+
+function App() {
+  return (
+    <Routes>
+      
+      <Route path="/" element={<Inicio />} />
+      <Route path="/sobre" element={<Sobre />} />
+      <Route path="/funcionalidades" element={<Funcionalidades />} />
+      <Route path="/comunicados" element={<Comunicados />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/teste" element={<Teste />} />
+      <Route path="/perfil" element={<Perfil />} />
+    </Routes>
+  );
+}
+
+export function WrappedApp() {
+  return (
+    <HashRouter>
+      <Navbar />
+      <body>
+        <App />
+      </body>
+      <Footer />
+    </HashRouter>
+  );
+}
---
+++ src/views/funcionalidades/Funcionalidades.jsx
@@ -0,0 +1,87 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import './style.css';
+import { css } from "@emotion/react";
+import { ClipLoader } from "react-spinners";
+
+const override = css`
+  display: block;
+  margin: 0 auto;
+  border-color: red;
+`;
+
+function Funcionalidades() {
+  const [loading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    setTimeout(() => {
+      axios
+        .get("https://m5-gru-crud-api.cyclic.app/adm/funcionalidades")
+        .then((response) => {
+          setData(response.data.data);
+          setLoading(false);
+        })
+        .catch((error) => {
+          console.error(error);
+          setLoading(false);
+        });
+    }, 0);
+  }, []);
+
+  return (
+    <div className="Funcionalidades">
+
+      {/* SPINNER LOADER DE CARREGAMENTO */}
+      {loading && (
+        <div
+          style={{
+            position: "absolute",
+            top: "50%",
+            left: "50%",
+            transform: "translate(-50%, -50%)",
+            zIndex: "9999",
+            backgroundColor: "#fff",
+            width: "100%",
+            height: "100%",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <ClipLoader color={"#36D7B7"} loading={loading} css={override} size={150} />
+        </div>
+      )}
+
+      {!loading && data && (
+        /* #################### */
+        <div>
+          {/* ↓↓↓ NÃO APAGAR NEM USAR ESSA DIV ↓↓↓ */}
+          <div className='nao_usar_essa_div'>&nbsp;</div>
+          {/* ↑↑↑ NÃO APAGAR NEM USAR ESSA DIV ↑↑↑ */}
+
+
+          {/* ↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓ POR O CÓDIGO AQUI ↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓ */}
+          {data.map(item => (
+
+
+            <div className='funcionalidades_bloco_do_card'>
+              <div className='funcionalidades_titulo' key={item.funcionalidade_titulo}>
+                <h2>{item.funcionalidade_titulo}</h2>
+                <p className='funcionalidades_descricao'>{item.funcionalidade_descricao}</p>
+              </div>
+            </div>
+
+          ))}
+
+
+          {/* ↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑ POR O CÓDIGO AQUI ↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑ */}
+        </div>
+      )}
+    </div>
+
+
+  );
+}
+
+export default Funcionalidades;
